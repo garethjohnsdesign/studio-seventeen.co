83c349554d6f335f46bea050aecca77f1f4adfe3
Gareth Johns <garethjohns@Gareths-iMac.local>
2019-10-28 15:31:52 +0000

    Site Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@ import AOS from 'aos';
 
 $(document).foundation();
 
+
 $('body').removeClass('fade-out');
 
 // 2. Animate on Scroll
@@ -24,9 +25,54 @@ window.addEventListener('load', AOS.refresh);
 });
 
 
+/*
 $("a.link").hover(function() {
-	$this = $(this);
-  $("body").css("background-image", "url(" + $(this).data("bg") + ")");
-	}, function() {
-	$("body").css("background-image", '');
+	var img = $(this).attr('data-image');
+	$("body").css('background-color', function() {
+		return $this.data('bgcolor');
+	});
+	$("body").css("background-image", "url(" + $(this).data("bg") + ")");
+});
+*/
+
+/*
+$("a.link").hover(function(){
+  $(this).css("background-color", "yellow");
+  }, function(){
+  $(this).css("background-color", "pink");
+});
+*/
+
+// Working
+
+/*
+$(".link").hover(function() {
+    $('body').css('background-image', "url("+$(this).attr('data-bg')+")");
+}, function() {
+    $('body').css('background-image', "");
+}); 
+*/
+
+
+$(".link").hover(function() {
+    var color = $(this).data('color');
+    var image = $(this).data('image');
+
+    $('body').css('background-color', color);
+    $('body').css('background-image', "url('" + image + "')");
+}, function() {
+    $('body').css('background-color', "");
+    $('body').css('background-image', "");
+});
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+const vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
